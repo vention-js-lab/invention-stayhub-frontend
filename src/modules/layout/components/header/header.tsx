f161af8822d46b79283c46915f6616dfafb8ac51
Aziza-Azizova <80139121+Aziza-Azizova@users.noreply.github.com>
2024-11-12 20:32:45 +0500

    [INV-35] Basic frontend layout (#1)

---
+++ src/modules/layout/components/header/header.tsx
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { ProfileOptions } from './profile-options';
+import { Logo } from './logo';
+import { style } from '../../styles/style';
+
+export function Header() {
+  return (
+    <Box sx={style.header}>
+      <Container maxWidth="xl">
+        <Box sx={style.headerContent}>
+          <Logo />
+          <ProfileOptions />
+        </Box>
+      </Container>
+    </Box>
+  );
+}
---
+++ src/modules/layout/components/header/user-menu.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Link from '@mui/material/Link';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { style } from '../../styles/style';
+
+export function UserMenu() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    setOpen((prev) => !prev);
+  };
+  return (
+    <Button sx={style.userMenu} onClick={handleClick}>
+      <Stack sx={style.userMenuContent}>
+        <MenuIcon fontSize="small" />
+        <AccountCircleIcon fontSize="medium" />
+      </Stack>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        sx={{ mt: 1 }}
+      >
+        <MenuItem>
+          <Link href="#" sx={{ width: 150 }}>
+            Log in
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link href="#" sx={{ width: 150 }}>
+            Sign up
+          </Link>
+        </MenuItem>
+      </Menu>
+    </Button>
+  );
+}
