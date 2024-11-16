3c7911fccc32737d6eb29e33930d5d5f4ae6480b
Suroj Sultonov <87817090+SurajSulten@users.noreply.github.com>
2024-11-16 12:51:49 +0500

    [INV-67] - Create accommodation card (#4)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,9 @@
 import { grey, pink } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const appTheme = createTheme({
   typography: {
