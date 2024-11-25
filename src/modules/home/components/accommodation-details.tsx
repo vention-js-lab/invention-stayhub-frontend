61aeed11a3115e1ff69d9ce6cbf8852b8ba58b88
SurajSulten <suraj.sulten@gmail.com>
2024-11-25 15:47:50 +0500

    feat: create accommodation details

---
+++ src/modules/home/components/accommodation-details.tsx
@@ -0,0 +1,70 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Icons } from '../constants/icons.constant';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { type AmenityKey, type AccommodationAmenity } from '#/modules/home/types/accommodation-amenity.type';
+import Divider from '@mui/material/Divider';
+import { type Accommodation } from '#/modules/home/types/accommodation.type';
+
+const amenitiesMap: Record<AmenityKey, { icon: React.ElementType; label: string }> = {
+  hasWifi: { icon: Icons.Wifi, label: 'WiFi' },
+  hasParking: { icon: Icons.Parking, label: 'Parking' },
+  hasSwimmingPool: { icon: Icons.SwimmingPool, label: 'Swimming Pool' },
+  hasPetAllowance: { icon: Icons.Pets, label: 'Pet Friendly' },
+  hasBackyard: { icon: Icons.Backyard, label: 'Backyard' },
+  hasSmokingAllowance: { icon: Icons.Smoking, label: 'Smoking Allowed' },
+  hasHospitalNearby: { icon: Icons.Hospital, label: 'Hospital Nearby' },
+  hasLaundryService: { icon: Icons.Laundry, label: 'Laundry Service' },
+  hasKitchen: { icon: Icons.Kitchen, label: 'Kitchen' },
+  hasAirConditioning: { icon: Icons.AirConditioning, label: 'Air Conditioning' },
+  hasTv: { icon: Icons.TV, label: 'TV' },
+  hasAirportTransfer: { icon: Icons.Airport, label: 'Airport Transfer' },
+  isCloseToCenter: { icon: Icons.City, label: 'Close to Center' },
+  isChildFriendly: { icon: Icons.ChildFriendly, label: 'Child Friendly' },
+  isQuietArea: { icon: Icons.QuietArea, label: 'Quiet Area' },
+};
+
+export function AccommodationDetails({ data }: { data: Accommodation }) {
+  const amenity = data.amenity;
+  const amenities = (Object.keys(amenity) as AmenityKey[])
+    .filter((key) => key in amenitiesMap && amenity[key])
+    .map((key) => amenitiesMap[key]);
+
+  return (
+    <Box sx={{ maxWidth: '55%' }}>
+      <Box sx={{ padding: '0 0 30px 16px' }}>
+        <Typography variant="h3" gutterBottom={true} sx={{ fontSize: '25px', marginBottom: '15px' }}>
+          {data.name}
+        </Typography>
+        <Box sx={{ display: 'flex', marginBottom: '10px' }}>
+          <Typography sx={{ padding: '0 8px 0 0' }}>{data.allowedNumberOfPeople} guests </Typography>
+          <Divider orientation="vertical" flexItem={true} />
+          <Typography>{data.numberOfRooms} rooms</Typography>
+          <Divider orientation="vertical" flexItem={true} />
+          <Typography>{data.squareMeters} square meters</Typography>
+        </Box>
+        <Typography sx={{}}>{data.description}</Typography>
+      </Box>
+      <Divider variant="middle" />
+      <Box sx={{ paddingTop: '30px' }}>
+        <Typography sx={{ paddingLeft: '16px', fontSize: '20px', fontWeight: 'bold' }}>What this place offers</Typography>
+        <List sx={{ display: 'flex', flexWrap: 'wrap' }}>
+          {amenities.map((offer, id) => {
+            const Icon = offer.icon;
+            return (
+              <ListItem key={id} sx={{ maxWidth: '50%' }}>
+                <ListItemIcon>
+                  <Icon />
+                </ListItemIcon>
+                <ListItemText primary={offer.label} />
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/modules/home/constants/icons.constant.ts
@@ -0,0 +1,33 @@
+import WifiIcon from '@mui/icons-material/Wifi';
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import PoolIcon from '@mui/icons-material/Pool';
+import PetsIcon from '@mui/icons-material/Pets';
+import YardIcon from '@mui/icons-material/Yard';
+import SmokingRoomsIcon from '@mui/icons-material/SmokingRooms';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import LocalLaundryServiceIcon from '@mui/icons-material/LocalLaundryService';
+import KitchenIcon from '@mui/icons-material/Kitchen';
+import HeatPumpIcon from '@mui/icons-material/HeatPump';
+import TvIcon from '@mui/icons-material/Tv';
+import LocalAirportIcon from '@mui/icons-material/LocalAirport';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import EscalatorWarningIcon from '@mui/icons-material/EscalatorWarning';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+
+export const Icons = {
+  Wifi: WifiIcon,
+  Parking: LocalParkingIcon,
+  SwimmingPool: PoolIcon,
+  Pets: PetsIcon,
+  Backyard: YardIcon,
+  Smoking: SmokingRoomsIcon,
+  Hospital: LocalHospitalIcon,
+  Laundry: LocalLaundryServiceIcon,
+  Kitchen: KitchenIcon,
+  AirConditioning: HeatPumpIcon,
+  TV: TvIcon,
+  Airport: LocalAirportIcon,
+  City: LocationCityIcon,
+  ChildFriendly: EscalatorWarningIcon,
+  QuietArea: VolumeDownIcon,
+};
