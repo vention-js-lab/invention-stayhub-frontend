761bbc965923873d54904ff1e6d20301d967a7d4
SurajSulten <suraj.sulten@gmail.com>
2024-11-26 09:38:27 +0500

    feat: add reservation card and move data

--- src/modules/home/components/accommodation-details.tsx
+++ src/modules/home/components/accommodation-details.tsx
@@ -5,7 +5,7 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { type AmenityKey, type AccommodationAmenity } from '#/modules/home/types/accommodation-amenity.type';
+import { type AmenityKey } from '#/modules/home/types/accommodation-amenity.type';
 import Divider from '@mui/material/Divider';
 import { type Accommodation } from '#/modules/home/types/accommodation.type';
 
@@ -28,9 +28,9 @@ const amenitiesMap: Record<AmenityKey, { icon: React.ElementType; label: string 
 };
 
 export function AccommodationDetails({ data }: { data: Accommodation }) {
-  const amenity = data.amenity;
-  const amenities = (Object.keys(amenity) as AmenityKey[])
-    .filter((key) => key in amenitiesMap && amenity[key])
+  const amenities = data.amenity;
+  const amenity = (Object.keys(amenities) as AmenityKey[])
+    .filter((key) => key in amenitiesMap && amenities[key])
     .map((key) => amenitiesMap[key]);
 
   return (
@@ -52,10 +52,10 @@ export function AccommodationDetails({ data }: { data: Accommodation }) {
       <Box sx={{ paddingTop: '30px' }}>
         <Typography sx={{ paddingLeft: '16px', fontSize: '20px', fontWeight: 'bold' }}>What this place offers</Typography>
         <List sx={{ display: 'flex', flexWrap: 'wrap' }}>
-          {amenities.map((offer, id) => {
+          {amenity.map((offer) => {
             const Icon = offer.icon;
             return (
-              <ListItem key={id} sx={{ maxWidth: '50%' }}>
+              <ListItem key={} sx={{ maxWidth: '50%' }}>
                 <ListItemIcon>
                   <Icon />
                 </ListItemIcon>
--- src/modules/home/routes/single-accommodation.route.tsx
+++ src/modules/home/routes/single-accommodation.route.tsx
@@ -1,121 +1,32 @@
-import { type AccommodationImage } from '#/modules/home/types/accommodation-image.type';
 import { AccommodationDetails } from '../components/accommodation-details';
 import { AccommodationImages } from '../components/accommodation-images';
-import AccomImage from '#/assets/images/card-temp-image.jpg';
 import { useAccommodationQuery } from '#/modules/home/api/single-accommodation.api';
 import { useParams } from 'react-router-dom';
+import { ReservationCard } from '../components/reservation-card';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-const MockData = {
-  id: '805fefa5-4757-447f-9228-1eac990f2189',
-  name: 'Lake House',
-  description: 'Serene house overlooking a picturesque lake, perfect for relaxation.',
-  coverImage: 'https://example.com/images/lakehouse.jpg',
-  price: 350.0,
-  available: true,
-  availableFrom: '2024-05-31T19:00:00.000Z',
-  availableTo: '2024-09-01T19:00:00.000Z',
-  squareMeters: 140,
-  numberOfRooms: 4,
-  allowedNumberOfPeople: 8,
-  createdAt: '2024-11-21T11:00:50.481Z',
-  updatedAt: '2024-11-21T11:00:50.481Z',
-  deletedAt: null,
-  ownerId: '18eb5efe-fa4e-4e7b-916a-42fd2511a360',
-  address: {
-    id: 'd4414073-bd76-4f52-803f-d123f3928270',
-    street: '50 Lakeside Rd',
-    city: 'Lake Tahoe',
-    country: 'USA',
-    zipCode: '96150',
-    latitude: '38.939900',
-    longitude: '-119.977200',
-    createdAt: '2024-11-21T11:00:50.500Z',
-    updatedAt: '2024-11-21T11:00:50.500Z',
-    accommodationId: '805fefa5-4757-447f-9228-1eac990f2189',
-  },
-  amenity: {
-    id: '3fc6ddb8-634b-4876-93e3-7f69a4677190',
-    hasWifi: true,
-    hasParking: true,
-    hasSwimmingPool: false,
-    hasPetAllowance: true,
-    hasBackyard: true,
-    hasSmokingAllowance: false,
-    hasHospitalNearby: false,
-    hasLaundryService: false,
-    hasKitchen: true,
-    hasAirConditioning: false,
-    hasTv: true,
-    hasAirportTransfer: false,
-    isCloseToCenter: false,
-    isChildFriendly: true,
-    isQuietArea: true,
-    createdAt: '2024-11-21T11:00:50.507Z',
-    updatedAt: '2024-11-21T11:00:50.507Z',
-    accommodationId: '805fefa5-4757-447f-9228-1eac990f2189',
-  },
-  images: [
-    {
-      id: 'b4ff34a3-febd-48a5-adbf-3d020a014b29',
-      url: 'https://example.com/images/lakehouse_view1.jpg',
-      createdAt: '2024-11-21T11:00:50.513Z',
-      updatedAt: '2024-11-21T11:00:50.513Z',
-      accommodationId: '805fefa5-4757-447f-9228-1eac990f2189',
-    },
-    {
-      id: '5b431efe-4755-4676-a2f7-ef5911ce4a64',
-      url: 'https://example.com/images/lakehouse_view2.jpg',
-      createdAt: '2024-11-21T11:00:50.513Z',
-      updatedAt: '2024-11-21T11:00:50.513Z',
-      accommodationId: '805fefa5-4757-447f-9228-1eac990f2189',
-    },
-  ],
-};
-const images: AccommodationImage[] = [
-  {
-    id: '1',
-    url: AccomImage,
-    createdAt: '2024-01-01',
-    updatedAt: '2024-01-01',
-    accommodationId: '101',
-  },
-  {
-    id: '2',
-    url: AccomImage,
-    createdAt: '2024-01-01',
-    updatedAt: '2024-01-01',
-    accommodationId: '101',
-  },
-  {
-    id: '3',
-    url: AccomImage,
-    createdAt: '2024-01-01',
-    updatedAt: '2024-01-01',
-    accommodationId: '101',
-  },
-  {
-    id: '4',
-    url: AccomImage,
-    createdAt: '2024-01-01',
-    updatedAt: '2024-01-01',
-    accommodationId: '101',
-  },
-  {
-    id: '5',
-    url: AccomImage,
-    createdAt: '2024-01-01',
-    updatedAt: '2024-01-01',
-    accommodationId: '101',
-  },
-];
 export function SingleAccommodationRoute() {
   const { id } = useParams<{ id: string }>();
   const { data } = useAccommodationQuery(id || '');
+  if (!data) {
+    return <Typography>No data is provided</Typography>;
+  }
 
   return (
     <>
-      <AccommodationImages images={images} />
-      <AccommodationDetails data={MockData} />
+      <AccommodationImages images={data.images} />
+      <Box sx={{ display: 'flex', gap: 10 }}>
+        <AccommodationDetails data={data} />
+        <ReservationCard
+          pricePerNight={data.price}
+          cleaningFee={35}
+          serviceFee={70}
+          maxGuests={data.allowedNumberOfPeople}
+          availableFrom="2024.10.04"
+          availableTo="2024.11.04"
+        />
+      </Box>
     </>
   );
 }
