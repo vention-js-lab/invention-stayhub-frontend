4d5bd52f436787b9b6ab675c31fe300596c674dd
Aziz Bektemirov <aziz.bektemirov64@gmail.com>
2024-11-30 14:54:57 +0500

    [INV-102] Import all weights of Roboto font for improved typography consistency

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { appTheme } from '#/styles/theme';
-import '@fontsource/roboto';
 import '#/styles/root.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { QueryClientConfig } from '#/configs/query-client.config';
@@ -14,6 +13,13 @@ import { router } from '#/routes';
 import { SnackbarProvider } from 'notistack';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+
+import '@fontsource/roboto/100.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
