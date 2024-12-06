63a24bc557cbe48baaf597f32eb565caf3b07036
SurajSulten <suraj.sulten@gmail.com>
2024-12-06 09:09:28 +0500

    feat: add routes

--- src/routes/private.route.tsx
+++ src/routes/private.route.tsx
@@ -1,11 +1,12 @@
 import { composeModuleRoutes } from '#/shared/utils/router.util';
 import { WishlistRoutes } from '#/modules/wishlist';
 import { ProtectedLayout } from '#/layout/components/protected-layout';
+import { AccountRoutes } from '#/modules/account';
 
 export const privateRoutes = [
   {
     path: '/',
     element: <ProtectedLayout />,
-    children: composeModuleRoutes(WishlistRoutes),
+    children: composeModuleRoutes(WishlistRoutes, AccountRoutes),
   },
 ];
--- src/routes/public.route.ts
+++ src/routes/public.route.ts
@@ -1,6 +1,5 @@
-import { ProfileRoutes } from '#/modules/account';
 import { AuthRoutes } from '#/modules/auth';
 import { AccommodationsRoutes } from '#/modules/accommodations';
 import { composeModuleRoutes } from '#/shared/utils/router.util';
 
-export const publicRoutes = composeModuleRoutes(AuthRoutes, AccommodationsRoutes, ProfileRoutes);
+export const publicRoutes = composeModuleRoutes(AuthRoutes, AccommodationsRoutes);
