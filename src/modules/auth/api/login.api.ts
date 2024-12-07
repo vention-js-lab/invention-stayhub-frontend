c270a70bf5e7d789a3ef0cb238731069bd3e34c9
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2024-12-07 21:08:04 +0500

    login, register api endpoints also changed to the same format

--- src/modules/auth/api/login.api.ts
+++ src/modules/auth/api/login.api.ts
@@ -3,14 +3,15 @@ import { apiClient } from '#/shared/libs/api-client.lib';
 import { type LoginFormData } from '../schemas/login-form.schema';
 import { type AxiosError } from 'axios';
 import { type LoginResponse } from '../types/login-response.type';
+import { type BaseResponse } from '#/shared/types/base-response.type';
 
 export function useLoginMutation() {
   const loginMutation = useMutation<LoginResponse, AxiosError, LoginFormData>({
     mutationFn: async (credentials) => {
-      const response = await apiClient.post<LoginResponse>('/auth/login', credentials);
-      return response.data;
+      const response = await apiClient.post<BaseResponse<LoginResponse>>('/auth/login', credentials);
+      return response.data.data;
     },
   });
 
-  return { loginMutation };
+  return loginMutation;
 }
