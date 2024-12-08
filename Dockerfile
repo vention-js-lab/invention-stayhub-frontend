73e25f147dcc8f76cb9e8fd128cd5e000ecde91c
Mukhammadjon Ubaydullaev <90293805+muhammadjon8@users.noreply.github.com>
2024-12-08 19:15:00 +0500

    [INV-94] - Fix all response format (#21)

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Step 1: Build Stage
-FROM node:18 AS build
+FROM node:22 AS build
 
 # Set the working directory
 WORKDIR /app
--- src/modules/auth/api/login.api.ts
+++ src/modules/auth/api/login.api.ts
@@ -2,15 +2,20 @@ import { useMutation } from '@tanstack/react-query';
 import { apiClient } from '#/shared/libs/api-client.lib';
 import { type LoginFormData } from '../schemas/login-form.schema';
 import { type AxiosError } from 'axios';
-import { type LoginResponse } from '../types/login-response.type';
+import { type BaseResponse } from '#/shared/types/base-response.type';
+
+interface LoginResponse {
+  accessToken: string;
+  refreshToken: string;
+}
 
 export function useLoginMutation() {
   const loginMutation = useMutation<LoginResponse, AxiosError, LoginFormData>({
     mutationFn: async (credentials) => {
-      const response = await apiClient.post<LoginResponse>('/auth/login', credentials);
-      return response.data;
+      const response = await apiClient.post<BaseResponse<LoginResponse>>('/auth/login', credentials);
+      return response.data.data;
     },
   });
 
-  return { loginMutation };
+  return loginMutation;
 }
