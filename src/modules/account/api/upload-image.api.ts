514a0559be5650592b6db22510bfb11d12b907fa
Suroj Sultonov <87817090+SurajSulten@users.noreply.github.com>
2024-12-10 13:46:54 +0500

    [INV-63] - Account and profile management page (#25)

---
+++ src/modules/account/api/upload-image.api.ts
@@ -0,0 +1,17 @@
+import { apiClient } from '#/shared/libs/api-client.lib';
+import { type BaseResponse } from '#/shared/types/base-response.type';
+import { useMutation } from '@tanstack/react-query';
+
+interface UploadResponse {
+  obJectUrl: string;
+}
+
+export function useUploadImageMutation() {
+  const mutation = useMutation<UploadResponse, Error, FormData>({
+    mutationFn: async (formdata: FormData) => {
+      const response = await apiClient.post<BaseResponse<UploadResponse>>('/uploads/image', formdata);
+      return response.data.data;
+    },
+  });
+  return mutation;
+}
---
+++ src/modules/account/routes/profile.route.tsx
@@ -0,0 +1,25 @@
+import Box from '@mui/material/Box';
+import { ProfileInfo } from '../components/profile';
+import { useProfileQuery } from '../api/get-profile.api';
+import Typography from '@mui/material/Typography';
+
+export function ProfileRoute() {
+  const { data } = useProfileQuery();
+  if (!data) {
+    return <Typography>{`User's data is not provided`}</Typography>;
+  }
+
+  return (
+    <Box>
+      <ProfileInfo
+        image={data.image}
+        firstName={data.firstName}
+        lastName={data.lastName}
+        phoneNumber={data.phoneNumber}
+        country={data.country}
+        gender={data.gender}
+        description={data.description}
+      />
+    </Box>
+  );
+}
