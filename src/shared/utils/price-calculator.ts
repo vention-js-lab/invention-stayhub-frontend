7cc8d20229e00dbfb9b6cd9d80f6b727d2e014f5
Savesh1508 <savlatxon20042005@gmail.com>
2024-12-11 22:30:29 +0500

    Fixed parsing decimal numbers

--- src/shared/utils/price-calculator.ts
+++ src/shared/utils/price-calculator.ts
@@ -4,7 +4,7 @@ import { SERVICE_FEE_PERCENTAGE } from '../constants/service-fee.constant';
 export function getPriceDetails(pricePerNight: number, startDate: string | Date, endDate: string | Date) {
   const numberOfNights = calculateDayDiff(startDate, endDate);
   const totalPriceOfNights = pricePerNight * numberOfNights;
-  const serviceFee = Number((totalPriceOfNights * SERVICE_FEE_PERCENTAGE).toFixed(1));
+  const serviceFee = Number((totalPriceOfNights * SERVICE_FEE_PERCENTAGE).toFixed(2));
   const totalPrice = totalPriceOfNights + serviceFee;
 
   return { numberOfNights, totalPriceOfNights, serviceFee, totalPrice };
