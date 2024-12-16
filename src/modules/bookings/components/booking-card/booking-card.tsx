5bfa9cc1df1be9bba430101f9bdf6bb2fb03fcbb
Savlatxon Eshonov <134992658+Savesh1508@users.noreply.github.com>
2024-12-17 00:14:33 +0500

    [INV-124] Create component for empty results (#32)

--- src/modules/bookings/components/booking-card/booking-card.tsx
+++ src/modules/bookings/components/booking-card/booking-card.tsx
@@ -1,12 +1,20 @@
 import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { CardSkeleton } from '#/modules/accommodations/components/accommodation-card/card-skeleton';
-import { type AccommodationAddress } from '#/modules/accommodations/types/accommodation-address.type';
 import Button from '@mui/material/Button';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import Link from '@mui/material/Link';
+import HomeIcon from '@mui/icons-material/Home';
+import RoomIcon from '@mui/icons-material/Room';
+import GradeIcon from '@mui/icons-material/Grade';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+import NightsStayIcon from '@mui/icons-material/NightsStay';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import SellIcon from '@mui/icons-material/Sell';
+import { type AccommodationAddress } from '#/modules/accommodations/types/accommodation-address.type';
+import { CardSkeleton } from '#/modules/accommodations/components/accommodation-card/card-skeleton';
 import { readableDate } from '#/shared/utils/readable-date.util';
 import { getPriceDetails } from '#/shared/utils/price-calculator';
 import { BookingStatus } from '#/modules/bookings/constants/booking-status.constant';
@@ -31,6 +39,10 @@ interface BookingCardProps {
 const styles = {
   bookingCard: { borderRadius: '15px', boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.4)', height: '100%' },
   cardPhoto: { width: '100%', maxHeight: '400px', backgroundColor: '#bdbdbd', objectFit: 'cover' },
+  actionButtons: { width: '100%', padding: '8px 10px', fontSize: '14px' },
+  mainText: { fontSize: '20px', fontWeight: '500', color: '#000', display: 'flex', alignItems: 'center' },
+  textStyle: { fontSize: '16px', pX: '0', display: 'flex', alignItems: 'center' },
+  iconStyle: { marginRight: '2px', fontSize: '20px' },
   cardTextArea: {
     width: '100%',
     display: 'flex',
@@ -38,9 +50,6 @@ const styles = {
     justifyContent: 'space-between',
     padding: '20px 23px 30px 23px',
   },
-  textStyle: { fontSize: '16px', pX: '0' },
-  actionButtons: { width: '100%', padding: '8px 10px', fontSize: '14px' },
-
   cardContent: {
     height: '100%',
     display: 'flex',
@@ -55,7 +64,6 @@ const styles = {
       flexDirection: 'column',
     },
   },
-
   notFoundImg: {
     width: '100%',
     height: '100%',
@@ -119,32 +127,56 @@ export function BookingCard({
           )}
           <Box sx={styles.cardTextArea}>
             <CardContent sx={{ padding: '0' }}>
-              <Typography sx={{ fontSize: '18px', fontWeight: '500' }}>{name}</Typography>
+              <Typography sx={{ ...styles.mainText }}>
+                <HomeIcon sx={{ marginRight: '2px', color: '#E91E63' }} />
+                {name}
+              </Typography>
               {address !== null && (
                 <Typography sx={styles.textStyle}>
+                  <RoomIcon sx={styles.iconStyle} />
                   Address: {address.city}, {address.street}
                 </Typography>
               )}
-              <Typography sx={styles.textStyle}>Rating: {rating}</Typography>
-              <Typography sx={styles.textStyle}>Check-in: {readableDate(startDate)}</Typography>
-              <Typography sx={styles.textStyle}>Checkout: {readableDate(endDate)}</Typography>
-              <Typography sx={styles.textStyle}>Nights: {numberOfNights}</Typography>
-              <Typography sx={styles.textStyle}>Price for nights: ${totalPriceOfNights}</Typography>
-              <Typography sx={styles.textStyle}>Stayhub fee: ${serviceFee}</Typography>
+              <Typography sx={styles.textStyle}>
+                <GradeIcon sx={styles.iconStyle} />
+                Rating: {rating}
+              </Typography>
+              <Typography sx={styles.textStyle}>
+                <LoginIcon sx={styles.iconStyle} />
+                Check-in: {readableDate(startDate)}
+              </Typography>
+              <Typography sx={styles.textStyle}>
+                <LogoutIcon sx={styles.iconStyle} />
+                Checkout: {readableDate(endDate)}
+              </Typography>
+              <Typography sx={styles.textStyle}>
+                <NightsStayIcon sx={styles.iconStyle} />
+                Nights: {numberOfNights}
+              </Typography>
+              <Typography sx={styles.textStyle}>
+                <AttachMoneyIcon sx={styles.iconStyle} />
+                Price for nights: ${totalPriceOfNights}
+              </Typography>
+              <Typography sx={styles.textStyle}>
+                <SellIcon sx={styles.iconStyle} />
+                Stayhub fee: ${serviceFee}
+              </Typography>
             </CardContent>
 
-            <CardActionArea sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
-              <Typography sx={{ fontSize: '16px', fontWeight: '500', display: 'flex', mb: '4px' }}>
+            <Box>
+              <Typography sx={{ ...styles.mainText, display: 'flex', mb: '4px', fontSize: '16px' }}>
                 Total Price:
                 <Typography sx={{ color: '#E91E63', fontSize: '16px', fontWeight: '500', px: '8px' }}>${totalPrice}</Typography>
               </Typography>
-              {bookingStatus !== BookingStatus.Canceled ? (
-                <>
+              <CardActionArea sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
+                {[BookingStatus.Pending, BookingStatus.Active].includes(bookingStatus) && (
                   <Link href={`/bookings/${id}/checkout`} sx={{ width: '100%', padding: '0' }}>
                     <Button variant="contained" color="secondary" sx={{ ...styles.actionButtons, marginBottom: '5px' }}>
                       Pay
                     </Button>
                   </Link>
+                )}
+                {[BookingStatus.Pending, BookingStatus.Active, BookingStatus.Inactive].includes(bookingStatus) && (
                   <Button
                     variant="contained"
                     sx={styles.actionButtons}
@@ -155,20 +187,28 @@ export function BookingCard({
                   >
                     Cancel
                   </Button>
-                </>
-              ) : (
-                <Button
-                  variant="contained"
-                  sx={{ ...styles.actionButtons, backgroundColor: '#00aa52' }}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleUpdateStatus(BookingStatus.Pending);
-                  }}
-                >
-                  Recover
-                </Button>
-              )}
-            </CardActionArea>
+                )}
+                {bookingStatus === BookingStatus.Canceled && (
+                  <Button
+                    variant="contained"
+                    sx={{ ...styles.actionButtons, backgroundColor: '#50BE55' }}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleUpdateStatus(BookingStatus.Pending);
+                    }}
+                  >
+                    Recover
+                  </Button>
+                )}
+                {bookingStatus === BookingStatus.Completed && (
+                  <Link href={`/bookings/${id}/review`} sx={{ width: '100%', padding: '0' }}>
+                    <Button variant="contained" color="secondary" sx={{ ...styles.actionButtons, marginBottom: '5px' }}>
+                      Add review
+                    </Button>
+                  </Link>
+                )}
+              </CardActionArea>
+            </Box>
           </Box>
         </Link>
       )}
