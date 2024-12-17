b8d75ceeaa03629d97cda84a029d9fc1fc9e594a
Aziza-Azizova <80139121+Aziza-Azizova@users.noreply.github.com>
2024-12-17 16:23:32 +0500

    [INV-120] Automatically log the user in when registered (#37)

--- src/modules/auth/api/register.api.ts
+++ src/modules/auth/api/register.api.ts
@@ -4,10 +4,15 @@ import { type RegisterFormData } from '../schemas/register-form.schema';
 import { type BaseResponse } from '#/shared/types/base-response.type';
 import { type AxiosError } from 'axios';
 
+interface RegisterResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
 export function useRegisterMutation() {
-  const mutation = useMutation<BaseResponse<null>, AxiosError, RegisterFormData>({
+  const mutation = useMutation<BaseResponse<RegisterResponse>, AxiosError, RegisterFormData>({
     mutationFn: async (data: RegisterFormData) => {
-      const response = await apiClient.post<BaseResponse<null>>('auth/register', data);
+      const response = await apiClient.post<BaseResponse<RegisterResponse>>('auth/register', data);
       return response.data;
     },
   });
