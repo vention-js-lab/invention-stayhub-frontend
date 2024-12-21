686c88ed504d0c65ee7c334354da10c653a18051
SurajSulten <suraj.sulten@gmail.com>
2024-12-21 19:07:38 +0500

    feat: make categories carousel responsive

--- src/modules/accommodations/components/categories/categories.tsx
+++ src/modules/accommodations/components/categories/categories.tsx
@@ -45,6 +45,29 @@ export function CategoriesCarousel() {
     arrows: true,
     prevArrow: <CustomPrevArrow />,
     nextArrow: <CustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   return (
     <Box sx={{ p: 3, gap: 1, position: 'relative' }}>
