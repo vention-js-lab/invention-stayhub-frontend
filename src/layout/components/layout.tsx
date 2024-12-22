cf9bbd6b6988bb34809b32b454fe25d404ce6a1d
SurajSulten <suraj.sulten@gmail.com>
2024-12-22 18:10:37 +0500

    feat: make profile, wishlist and footer responsive

--- src/layout/components/layout.tsx
+++ src/layout/components/layout.tsx
@@ -11,6 +11,12 @@ export function Layout() {
         flexDirection: 'column',
         minHeight: '100vh',
         paddingX: 15,
+        '@media (max-width: 960px)': {
+          paddingX: 10,
+        },
+        '@media (max-width: 600px)': {
+          paddingX: 4,
+        },
       }}
     >
       <Header />
